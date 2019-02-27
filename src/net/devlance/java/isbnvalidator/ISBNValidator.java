e25bc336dbe186502750f130c7dda76b736d5e85
Yassine El Houari <yas.elhouari@gmail.com>
2019-02-27 14:59:56 +0100

    Create the test for thirteen digits ISBN Numbers and make it pass

--- src/net/devlance/java/isbnvalidator/ISBNValidator.java
+++ src/net/devlance/java/isbnvalidator/ISBNValidator.java
@@ -3,20 +3,35 @@ package net.devlance.java.isbnvalidator;
 public class ISBNValidator {
     public boolean checkISBN(String isbn) {
         int total = 0;
-        if(isbn.length() != 10 ) throw new NumberFormatException ("Less than 10 Digits are not allowed");
-        for(int i=0; i<10; i++) {
-            if(!Character.isDigit(isbn.charAt(i))) {
-                if(i == 9 && isbn.charAt(i) == 'X') {
-                    total+= 10;
+        if (isbn.length () == 13) {
+            for(int i=0; i<13; i++) {
+                if(i%2 == 0) {
+                    total += Character.getNumericValue ( isbn.charAt ( i ) );
                 } else {
-                    throw new NumberFormatException ( "ISBN Numbers can only contain digits" );
+                    total += Character.getNumericValue ( isbn.charAt ( i ) ) * 3;
                 }
-            } else {
-                total += Character.getNumericValue(isbn.charAt(i) ) * (10 - i);
             }
+            if(total%10 == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (isbn.length () != 10) throw new NumberFormatException ( "Less than 10 Digits are not allowed" );
+            for (int i = 0; i < 10; i++) {
+                if (!Character.isDigit ( isbn.charAt ( i ) )) {
+                    if (i == 9 && isbn.charAt ( i ) == 'X') {
+                        total += 10;
+                    } else {
+                        throw new NumberFormatException ( "ISBN Numbers can only contain digits" );
+                    }
+                } else {
+                    total += Character.getNumericValue ( isbn.charAt ( i ) ) * (10 - i);
+                }
+            }
+            if (total % 11 == 0)
+                return true;
+            else return false;
         }
-        if(total%11 ==0)
-            return true;
-        else return false;
     }
 }
--- test/net/devlance/java/isbnvalidator/ISBNValidatorTest.java
+++ test/net/devlance/java/isbnvalidator/ISBNValidatorTest.java
@@ -1,12 +1,11 @@
 package net.devlance.java.isbnvalidator;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class ISBNValidatorTest {
 
     @Test
-    public void checkValidISBN() {
+    public void checkTenDigitsValidISBN() {
         ISBNValidator isbnValidator = new ISBNValidator();
         boolean result = isbnValidator.checkISBN("0140449116");
         assertTrue("first value of ISBN", result);
@@ -15,13 +14,13 @@ public class ISBNValidatorTest {
         assertTrue("second value of ISBN", result);
     }
     @Test
-    public void isbnNumbersEndingInAnXAreValid() {
+    public void isbnTenDigitsNumbersEndingInAnXAreValid() {
         ISBNValidator isbnValidator = new ISBNValidator();
         boolean result = isbnValidator.checkISBN("012000030X");
         assertTrue(result);
     }
     @Test
-    public void checkInvalidISBN() {
+    public void checkTenDigitsInvalidISBN() {
         ISBNValidator isbnValidator = new ISBNValidator();
         boolean result = isbnValidator.checkISBN("0140449117");
         assertFalse(result);
@@ -37,5 +36,19 @@ public class ISBNValidatorTest {
         ISBNValidator isbnValidator = new ISBNValidator ();
         boolean result = isbnValidator.checkISBN ( "helloworld" );
         assertFalse ( result );
+    }
+    @Test
+    public void checkValidThirteenDigitsISBNNumbers() {
+        ISBNValidator isbnValidator = new ISBNValidator();
+        boolean result = isbnValidator.checkISBN("9781853267338");
+        assertTrue("First 13 digits value", result);
+        result = isbnValidator.checkISBN("9783161484100");
+        assertTrue("Second 13 digits value", result);
+    }
+    @Test
+    public void checkInvalidThirteenDigitsISBNNumbers() {
+        ISBNValidator isbnValidator = new ISBNValidator();
+        boolean result = isbnValidator.checkISBN("9781853260085");
+        assertFalse("Invalid 13 digits ISBN Number", result);
     }
 }
