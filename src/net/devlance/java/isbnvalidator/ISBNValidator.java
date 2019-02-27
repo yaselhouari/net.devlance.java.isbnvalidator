cb8d9255feeb2dd80c4d2b1deb6396013b4f64f7
Yassine El Houari <yas.elhouari@gmail.com>
2019-02-27 20:58:26 +0100

    add some refactoring to clean code

--- src/net/devlance/java/isbnvalidator/ISBNValidator.java
+++ src/net/devlance/java/isbnvalidator/ISBNValidator.java
@@ -4,34 +4,43 @@ public class ISBNValidator {
     public boolean checkISBN(String isbn) {
         int total = 0;
         if (isbn.length () == 13) {
-            for(int i=0; i<13; i++) {
-                if(i%2 == 0) {
-                    total += Character.getNumericValue ( isbn.charAt ( i ) );
-                } else {
-                    total += Character.getNumericValue ( isbn.charAt ( i ) ) * 3;
-                }
-            }
-            if(total%10 == 0) {
-                return true;
-            } else {
-                return false;
-            }
+            return isThisValidThirteenDigitsISBN ( isbn, total );
+        } else if(isbn.length () == 10){
+            return isThisValidTenDigitsISBN ( isbn, total );
         } else {
-            if (isbn.length () != 10) throw new NumberFormatException ( "Less than 10 Digits are not allowed" );
-            for (int i = 0; i < 10; i++) {
-                if (!Character.isDigit ( isbn.charAt ( i ) )) {
-                    if (i == 9 && isbn.charAt ( i ) == 'X') {
-                        total += 10;
-                    } else {
-                        throw new NumberFormatException ( "ISBN Numbers can only contain digits" );
-                    }
+            throw new NumberFormatException ( "Digits that are not 10 or 13 are not allowed!" );
+        }
+    }
+    private boolean isThisValidTenDigitsISBN(String isbn, int total) {
+        for (int i = 0; i < 10; i++) {
+            if (!Character.isDigit ( isbn.charAt ( i ) )) {
+                if (i == 9 && isbn.charAt ( i ) == 'X') {
+                    total += 10;
                 } else {
-                    total += Character.getNumericValue ( isbn.charAt ( i ) ) * (10 - i);
+                    throw new NumberFormatException ( "ISBN Numbers can only contain digits" );
                 }
+            } else {
+                total += Character.getNumericValue ( isbn.charAt ( i ) ) * (10 - i);
             }
-            if (total % 11 == 0)
-                return true;
-            else return false;
+        }
+        if (total % 11 == 0)
+            return true;
+        else
+            return false;
+    }
+
+    private boolean isThisValidThirteenDigitsISBN(String isbn, int total) {
+        for(int i=0; i<13; i++) {
+            if(i%2 == 0) {
+                total += Character.getNumericValue ( isbn.charAt ( i ) );
+            } else {
+                total += Character.getNumericValue ( isbn.charAt ( i ) ) * 3;
+            }
+        }
+        if(total%10 == 0) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
